0ac0f1236441df1e24ca0a095f1d2a67f38388e2
Dennis <>
2020-07-19 01:45:28 +0200

    some changes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import time
 import csv
+import cv2
 import numpy as np
 
 import torch
@@ -30,11 +31,8 @@ def main():
     print("=> creating data loaders...")
     valdir = os.path.join('..', 'data', args.data, 'val')
 
-    if args.data == 'nyudepthv2':
-        from dataloaders.nyu import NYUDataset
-        val_dataset = NYUDataset(valdir, split='val', modality=args.modality)
-    else:
-        raise RuntimeError('Dataset not found.')
+    val_dataset = torch.Tensor(cv2.divide(cv2.resize(cv2.imread("data/nyudepthv2/val/0000000140.jpg"), (128, 128)), 255.))
+    val_dataset = val_dataset.unsqueeze(0)
 
     # set batch size to be 1 for validation
     val_loader = torch.utils.data.DataLoader(val_dataset,
@@ -42,30 +40,23 @@ def main():
     print("=> data loaders created.")
 
     # evaluation mode
-    if args.evaluate:
-        assert os.path.isfile(args.evaluate), \
-        "=> no model found at '{}'".format(args.evaluate)
-        print("=> loading model '{}'".format(args.evaluate))
-        checkpoint = torch.load(args.evaluate)
-        if type(checkpoint) is dict:
-            args.start_epoch = checkpoint['epoch']
-            best_result = checkpoint['best_result']
-            model = checkpoint['model']
-            print("=> loaded best model (epoch {})".format(checkpoint['epoch']))
-        else:
-            model = checkpoint
-            args.start_epoch = 0
-        output_directory = os.path.dirname(args.evaluate)
-        validate(val_loader, model, args.start_epoch, write_to_file=False)
-        return
+    model = models.MobileNetSkipAdd((640, 480))
+    # model = model.load_state_dict(torch.load('model_best.pth.tar')['state_dict'])
+    args.start_epoch = 0
+    validate(val_loader, model, args.start_epoch, write_to_file=False)
 
 
 def validate(val_loader, model, epoch, write_to_file=True):
     average_meter = AverageMeter()
-    model.eval() # switch to evaluate mode
+    model.eval()
     end = time.time()
-    for i, (input, target) in enumerate(val_loader):
-        input, target = input.cuda(), target.cuda()
+    i = 0
+    cam = cv2.VideoCapture("http://192.168.178.195:4747/mjpegfeed?640x480")
+    while True:
+        input = cv2.resize(cam.read()[1], (640, 480))
+        cv2.imshow("IN", input)
+        print(input)
+        input = torch.Tensor(np.reshape(input, [3, 480, 640])).unsqueeze(0).float()
         # torch.cuda.synchronize()
         data_time = time.time() - end
 
@@ -76,55 +67,11 @@ def validate(val_loader, model, epoch, write_to_file=True):
         # torch.cuda.synchronize()
         gpu_time = time.time() - end
 
-        # measure accuracy and record loss
-        result = Result()
-        result.evaluate(pred.data, target.data)
-        average_meter.update(result, gpu_time, data_time, input.size(0))
-        end = time.time()
+        pred = np.array(pred.squeeze(0))*10.
+        print(pred)
 
-        # save 8 images for visualization
-        skip = 50
-
-        if args.modality == 'rgb':
-            rgb = input
-
-        if i == 0:
-            img_merge = utils.merge_into_row(rgb, target, pred)
-        elif (i < 8*skip) and (i % skip == 0):
-            row = utils.merge_into_row(rgb, target, pred)
-            img_merge = utils.add_row(img_merge, row)
-        elif i == 8*skip:
-            filename = output_directory + '/comparison_' + str(epoch) + '.png'
-            utils.save_image(img_merge, filename)
-
-        if (i+1) % args.print_freq == 0:
-            print('Test: [{0}/{1}]\t'
-                  't_GPU={gpu_time:.3f}({average.gpu_time:.3f})\n\t'
-                  'RMSE={result.rmse:.2f}({average.rmse:.2f}) '
-                  'MAE={result.mae:.2f}({average.mae:.2f}) '
-                  'Delta1={result.delta1:.3f}({average.delta1:.3f}) '
-                  'REL={result.absrel:.3f}({average.absrel:.3f}) '
-                  'Lg10={result.lg10:.3f}({average.lg10:.3f}) '.format(
-                   i+1, len(val_loader), gpu_time=gpu_time, result=result, average=average_meter.average()))
-
-    avg = average_meter.average()
-
-    print('\n*\n'
-        'RMSE={average.rmse:.3f}\n'
-        'MAE={average.mae:.3f}\n'
-        'Delta1={average.delta1:.3f}\n'
-        'REL={average.absrel:.3f}\n'
-        'Lg10={average.lg10:.3f}\n'
-        't_GPU={time:.3f}\n'.format(
-        average=avg, time=avg.gpu_time))
-
-    if write_to_file:
-        with open(test_csv, 'a') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writerow({'mse': avg.mse, 'rmse': avg.rmse, 'absrel': avg.absrel, 'lg10': avg.lg10,
-                'mae': avg.mae, 'delta1': avg.delta1, 'delta2': avg.delta2, 'delta3': avg.delta3,
-                'data_time': avg.data_time, 'gpu_time': avg.gpu_time})
-    return avg, img_merge
+        cv2.imshow("PRED", cv2.resize(np.reshape(pred, [480, 640, 1]), (640, 480)))
+        cv2.waitKey(1)
 
 if __name__ == '__main__':
     main()
